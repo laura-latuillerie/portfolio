7a22e3589025c754fac896cf08212fb88ebb5445
laura-latuillerie <laura.latuillerie@gmail.com>
2022-04-06 20:57:44 +0200

    Add Stats Circle Progress Bar

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ new TypeIt("#typing-demo", {
   speed: 75,
   loop: true,
 }).go();
+
+$(function () {
+  $(".chart").easyPieChart({
+    size: 100,
+    barColor: "darkred",
+    scaleLength: 0,
+    lineWidth: 10,
+    trackColor: "#eeeeee",
+    lineCap: "circle",
+    animate: 3000,
+  });
+});
