df2e244792e69ba5cf2d05a72bc9ade6cc8db0b3
laura-latuillerie <laura.latuillerie@gmail.com>
2022-04-06 05:06:15 +0200

    Create Basis + Upload Media + Work on Banner

---
+++ script.js
@@ -0,0 +1,11 @@
+new TypeIt("#typing-demo", {
+  strings: [
+    "Hello, I'm Laura !",
+    "A passionate...",
+    "👩🏻‍🎤 Designer",
+    "🤖 Developer",
+    "👽 Human being",
+  ],
+  speed: 50,
+  waitUntilVisible: true,
+}).go();
