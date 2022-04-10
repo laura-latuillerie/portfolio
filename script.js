2802284cf28584d2b386aee1f6c8900e95644f58
laura-latuillerie <laura.latuillerie@gmail.com>
2022-04-10 08:24:56 +0200

    Create footer and Update links

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ window.onload = function () {
 
 new TypeIt(".typing-effect", {
   strings: "",
-  speed: 80,
-  waitUntilVisible: true,
-}).go();
+  speed: 100,
+  loop: true,
+})
+  .type("Hello, I'm Laura !", { delay: 100 })
+  .delete(18, { delay: 800 })
+  .type("私はローラです", { delay: 100 })
+  .delete(7, { delay: 800 })
+  .type("ᕦ(ò_óˇ)ᕤ", { delay: 100 })
+  .delete(8, { delay: 800 })
+  .go();
